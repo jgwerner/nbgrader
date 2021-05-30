d03da89258a5c64697bb4d5b5cefd6f74568e101
Greg Werner <werner.greg@gmail.com>
2021-05-29 23:07:40 -0400

    upgrade db with course_label
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

---
+++ nbgrader/alembic/versions/508ca9f242ba_add_course_label_column.py
@@ -0,0 +1,24 @@
+"""add course_label column
+
+Revision ID: 508ca9f242ba
+Revises: e43177bfe90b
+Create Date: 2021-05-29 22:34:51.135666
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '508ca9f242ba'
+down_revision = 'e43177bfe90b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('course', sa.Column('course_label', sa.Float))
+
+
+def downgrade():
+    op.drop_column('course', 'course_label')
